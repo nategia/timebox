53946398e1b1413b0cb763b8fc9f28da18d2638c
Nate <nate@calculated.fi>
2024-09-13 13:19:41 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const queryClient = new QueryClient();
+
+const rootElement = document.getElementById("app")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,15 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <div className="p-8">
+      <div className="bg-amber-200 p-8 w-full rounded-md text-center">
+        <h1 className="text-gray-800">Timebox</h1>
+      </div>
+    </div>
+  );
+}
