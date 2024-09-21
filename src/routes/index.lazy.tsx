4fa073353401dae41b79a6b42b1888ae87ad3930
Nate <nate@calculated.fi>
2024-09-22 00:26:33 +0900

    minimalist style

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/button";
 import { Input } from "@/components/input";
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { ChevronsUpDown, Grip } from "lucide-react";
+import { Grip } from "lucide-react";
 import { useEffect, useState } from "react";
-import { LuCheck, LuPenSquare, LuTrash2, LuUndo } from "react-icons/lu";
+import {
+  LuCheck,
+  LuFileQuestion,
+  LuPenSquare,
+  LuTrash2,
+  LuUndo,
+} from "react-icons/lu";
 import { create } from "zustand";
 import {
   DragDropContext,
@@ -12,13 +18,17 @@ import {
   DropResult,
 } from "react-beautiful-dnd";
 import { persist } from "zustand/middleware";
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/collapsible";
 import { slice } from "ramda";
 import { Progress } from "@/components/progress";
+import { Badge } from "@/components/badge";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/dialog";
 
 type TodoStore = {
   todos: Array<{ id: number; content: string; completed: boolean }>;
@@ -76,47 +86,34 @@ const useTodoStore = create(
 
 const Instructions = () => {
   return (
-    <Collapsible>
-      <div className="p-4 bg-gray-100 rounded-md text-left space-y-2">
-        <CollapsibleTrigger asChild>
-          <Button
-            variant="ghost"
-            className="flex flex-row justify-between w-full"
-          >
-            <h2 className="text-xl font-bold">How to Use Timebox</h2>
-            <ChevronsUpDown />
-          </Button>
-        </CollapsibleTrigger>
-        <CollapsibleContent className="space-y-2">
-          <p>
-            1. <strong>Add Tasks:</strong> Type your task in the input field and
-            press <em>Enter</em> or click <em>Add Todo</em>.
-          </p>
-          <p>
-            2. <strong>Reorder Tasks:</strong> Click and drag the grip icon to
-            reorder tasks by priority.
-          </p>
-          <p>
-            3. <strong>Complete Tasks:</strong> Click the checkmark to mark a
-            task as complete.
-          </p>
-          <p>
-            4. <strong>Remove Tasks:</strong> Click the trash can to remove a
-            task from the list.
-          </p>
-          <p>
-            5. <strong>Clear All Tasks:</strong> Click <em>Scrap all</em> to
-            clear the entire list and start fresh.
-          </p>
-        </CollapsibleContent>
-      </div>
-    </Collapsible>
+    <div className=" space-y-4 py-4">
+      <p>
+        1. <strong>Add Tasks:</strong> Type your task in the input field and
+        press <em>Enter</em> or click <em>Add Todo</em>.
+      </p>
+      <p>
+        2. <strong>Reorder Tasks:</strong> Click and drag the grip icon to
+        reorder tasks by priority.
+      </p>
+      <p>
+        3. <strong>Complete Tasks:</strong> Click the checkmark to mark a task
+        as complete.
+      </p>
+      <p>
+        4. <strong>Remove Tasks:</strong> Click the trash can to remove a task
+        from the list.
+      </p>
+      <p>
+        5. <strong>Clear All Tasks:</strong> Click <em>Scrap all</em> to clear
+        the entire list and start fresh.
+      </p>
+    </div>
   );
 };
 
 const Details = () => {
   return (
-    <p className="p-4 text-sm text-muted-foreground text-left w-full">
+    <p className=" text-xs text-muted-foreground text-left w-full">
       This is a simple todo app that uses{" "}
       <a
         href="https://tanstack.com/query/v4"
@@ -156,6 +153,40 @@ export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
+const Header = () => (
+  <div className="flex flex-row justify-between items-center p-4  w-full ">
+    <h1 className="text-2xl font-bold">Timebox.</h1>
+    <Dialog>
+      <DialogTrigger>
+        <Button variant="outline">
+          <div className="space-x-2 flex flex-row items-center">
+            <p>Instructions</p>
+            <LuFileQuestion />
+          </div>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>How to use Timebox</DialogTitle>
+        </DialogHeader>
+        <Instructions />
+        <DialogFooter>
+          <Details />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  </div>
+);
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="bg-background w-full h-full">
+      <Header />
+      <center>{children}</center>
+    </div>
+  );
+};
+
 function Index() {
   const { todos, addTodo, removeTodo, cleanup, reorderTodos, complete } =
     useTodoStore();
@@ -180,157 +211,173 @@ function Index() {
   }, [currProgressPercent]);
 
   return (
-    <div className="p-0 sm:p-8 min-h-screen bg-background flex-col flex flex-grow ">
-      <div className="bg-background p-4 sm:p-8 w-full rounded-md text-center space-y-4">
-        <h1 className="p-4 text-2xl font-bold">Don't waste time, Timebox.</h1>
-        <Instructions />
-        <Details />
-        <div className="flex flex-col sm:flex-row w-full items-center justify-center space-x-0 space-y-2 sm:space-x-4 sm:space-y-0 bg-background">
-          <Input
-            placeholder="brain dump of what you need to do today"
-            onChange={(e) => setNewTodo(e.target.value)}
-            value={newTodo}
-            className="w-full"
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                e.preventDefault();
-                if (newTodo.trim()) {
-                  addTodo(newTodo);
-                  setNewTodo("");
+    <Layout>
+      <div className="p-0 sm:p-8 bg-background space-y-4 flex-col flex max-w-screen-lg">
+        <div className="px-4 py-8 text-center space-y-4">
+          <div className="flex flex-col sm:flex-row w-full items-center justify-center p-4 space-x-0 space-y-2 sm:space-x-4 sm:space-y-0">
+            <Input
+              placeholder="Brain dump of what you need to do today"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+              className="w-full bg-white h-12"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                  if (newTodo.trim()) {
+                    addTodo(newTodo);
+                    setNewTodo("");
+                  }
                 }
-              }
-            }}
-          />
-          <Button
-            onClick={() => {
-              addTodo(newTodo);
-              setNewTodo("");
-            }}
-            type="submit"
-            disabled={!newTodo}
-            className=" w-full sm:w-auto"
-          >
-            Add Todo
-          </Button>
-          <Button
-            onClick={cleanup}
-            variant="secondary"
-            className=" w-full sm:w-auto"
-          >
-            Scrap all
-          </Button>
+              }}
+            />
+            <Button
+              onClick={() => {
+                addTodo(newTodo);
+                setNewTodo("");
+              }}
+              type="submit"
+              disabled={!newTodo}
+              className=" w-full sm:w-auto"
+            >
+              Add Todo
+            </Button>
+            <Button
+              onClick={cleanup}
+              variant="secondary"
+              className=" w-full sm:w-auto"
+            >
+              Scrap all
+            </Button>
+          </div>
         </div>
-        <div className="w-full border-secondary-foreground border space-y-2 p-4">
-          <h2 className="p-4 text-xl font-bold">Your Progress</h2>
-          <Progress value={progress} className="w-full" />
-        </div>
-        <DragDropContext onDragEnd={handleDragEnd}>
-          <Droppable droppableId="droppable">
-            {(provided) => (
-              <ul
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                className="space-y-2"
-              >
-                {todos?.map(({ id, content, completed }, index) => {
-                  return (
-                    <Draggable
-                      key={id}
-                      draggableId={id.toString()}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          className="p-4 flex items-center gap-4 border bg-amber-100"
+
+        {todos.length > 0 && (
+          <div className="flex flex-col space-y-4 p-4">
+            <div className="flex flex-row items-center justify-between space-x-8 p-4">
+              <h3 className="text-lg font-bold">Progress:</h3>
+
+              <div className="flex flex-row justify-end items-center space-x-2  w-1/2">
+                <Progress value={progress} className="rounded-none" />
+                <Badge>{progress ? progress.toFixed(0) : 0}%</Badge>
+              </div>
+            </div>
+            <DragDropContext onDragEnd={handleDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided) => (
+                  <ul
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="space-y-2"
+                  >
+                    {todos?.map(({ id, content, completed }, index) => {
+                      return (
+                        <Draggable
+                          key={id}
+                          draggableId={id.toString()}
+                          index={index}
                         >
-                          <div color="secondary">
-                            <Grip />
-                          </div>
-                          <h2 className="text-xl font-bold">{index + 1}</h2>
-                          <div className="flex flex-row w-full text-start p-4 h-full">
-                            {editTodo === id ? (
-                              <Input
-                                onChange={(e) => setEditContent(e.target.value)}
-                                value={editContent}
-                                className="w-full"
-                                autoFocus
-                                onKeyDown={(e) => {
-                                  if (e.key === "Enter") {
-                                    e.preventDefault();
-                                    if (editTodo !== null) {
-                                      if (editContent.trim() !== "") {
-                                        useTodoStore.setState((state) => ({
-                                          todos: state.todos.map((todo) =>
-                                            todo.id === id
-                                              ? {
-                                                  ...todo,
-                                                  content: editContent,
-                                                }
-                                              : todo
-                                          ),
-                                        }));
-                                        setEditTodo(null);
-                                        setEditContent("");
-                                      }
+                          {(provided) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className="p-4 flex items-center gap-4"
+                            >
+                              <div color="secondary">
+                                <Grip />
+                              </div>
+                              <h2 className="text-lg font-bold">{index + 1}</h2>
+                              <div className="flex flex-row w-full text-start h-full">
+                                {editTodo === id ? (
+                                  <Input
+                                    onChange={(e) =>
+                                      setEditContent(e.target.value)
                                     }
-                                  }
-                                }}
-                              />
-                            ) : (
-                              <p
-                                style={{
-                                  textDecoration: completed
-                                    ? "line-through"
-                                    : "none",
-                                }}
-                                className="text-sm sm:text-lg"
-                              >
-                                {capitaliseFirstLetter(content)}
-                              </p>
-                            )}
-                          </div>
-                          <div className="flex flex-col sm:flex-row items-center justify-center space-x-0 space-y-2 sm:space-x-2 sm:space-y-0">
-                            <Button
-                              size="icon"
-                              onClick={() => {
-                                setEditTodo(id === editTodo ? null : id);
-                                setEditContent(capitaliseFirstLetter(content));
-                              }}
-                              className="bg-blue-500 hover:bg-blue-600/90 "
-                            >
-                              {id === editTodo ? <LuUndo /> : <LuPenSquare />}
-                            </Button>
-                            <Button
-                              variant="outline"
-                              size="icon"
-                              onClick={() => complete(id)}
-                            >
-                              {completed ? <LuUndo /> : <LuCheck />}
-                            </Button>
-                            <Button
-                              variant="destructive"
-                              size="icon"
-                              onClick={() => {
-                                removeTodo(id);
-                              }}
-                            >
-                              <LuTrash2 />
-                            </Button>
-                          </div>
-                        </div>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
+                                    value={editContent}
+                                    className="w-full"
+                                    autoFocus
+                                    onKeyDown={(e) => {
+                                      if (e.key === "Enter") {
+                                        e.preventDefault();
+                                        if (editTodo !== null) {
+                                          if (editContent.trim() !== "") {
+                                            useTodoStore.setState((state) => ({
+                                              todos: state.todos.map((todo) =>
+                                                todo.id === id
+                                                  ? {
+                                                      ...todo,
+                                                      content: editContent,
+                                                    }
+                                                  : todo
+                                              ),
+                                            }));
+                                            setEditTodo(null);
+                                            setEditContent("");
+                                          }
+                                        }
+                                      }
+                                    }}
+                                  />
+                                ) : (
+                                  <p
+                                    style={{
+                                      textDecoration: completed
+                                        ? "line-through"
+                                        : "none",
+                                    }}
+                                    className="text-sm sm:text-lg"
+                                  >
+                                    {capitaliseFirstLetter(content)}
+                                  </p>
+                                )}
+                              </div>
+                              <div className="flex flex-col sm:flex-row items-center justify-center space-x-0 space-y-2 sm:space-x-2 sm:space-y-0">
+                                <Button
+                                  size="icon"
+                                  variant="outline"
+                                  onClick={() => {
+                                    setEditTodo(id === editTodo ? null : id);
+                                    setEditContent(
+                                      capitaliseFirstLetter(content)
+                                    );
+                                  }}
+                                >
+                                  {id === editTodo ? (
+                                    <LuUndo />
+                                  ) : (
+                                    <LuPenSquare />
+                                  )}
+                                </Button>
+                                <Button
+                                  variant="outline"
+                                  size="icon"
+                                  onClick={() => complete(id)}
+                                >
+                                  {completed ? <LuUndo /> : <LuCheck />}
+                                </Button>
+                                <Button
+                                  variant="destructive"
+                                  size="icon"
+                                  onClick={() => {
+                                    removeTodo(id);
+                                  }}
+                                >
+                                  <LuTrash2 />
+                                </Button>
+                              </div>
+                            </div>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div>
+        )}
       </div>
-    </div>
+    </Layout>
   );
 }
